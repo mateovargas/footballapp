4c240ce6366a06ff36b123d4eddaea0f6120f926
Mateo Vargas <mateovargas@Mateos-MBP.san.rr.com>
2017-12-28 16:53:41 -0800

    Added player and fixture tables.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ var competitions = '';
 var next_fixtures = [];
 var modal = $('#team-view');
 var modal_flag = false;
+var current_team = '';
 modal.hide();
 
 
@@ -88,6 +89,7 @@ function displayTeamInfo(){
   modal.show();
 
   var team_name = $(this).attr('team-name');
+  current_team = team_name;
   var fixture_number = $(this).attr('fixture-number');
 
   console.log(next_fixtures[fixture_number]);
@@ -152,13 +154,106 @@ function displayForm(){
     }).done(function(response){
 
       $('#team-view').empty();
+      modal.show();
       console.log(response);
 
+      var table_div = $('<div>');
+      table_div.addClass('modal-content');
+      table_div.append('<span class="close">&times;</span>');
+
+      var fixture_table = $('<table>');
+      fixture_table.attr('id', 'player-table');
+      fixture_table.append('<tr><th>Match Day</th><th>Date</th><th>Home</th><th>Away</th><th>Home Goals</th><th>Away Goals</th><th>Result</th></tr>');
+
+      for(var i = 0; i < response.fixtures.length; i++){
+
+        if(response.fixtures[i].status == 'TIMED' || response.fixtures[i].status == 'SCHEDULED'){
+          break;
+        }
+
+        var matchday = response.fixtures[i].matchday;
+        var date = response.fixtures[i].date;
+        var home_team = response.fixtures[i].homeTeamName;
+        var away_team = response.fixtures[i].awayTeamName;
+        var result = response.fixtures[i].result;
+        var half_result = response.fixtures[i].result.halfTime;
+
+        var fixture_row = $('<tr>');
+        fixture_row.append('<th>' + matchday + '</th>');
+        fixture_row.append('<th>' + date + '</th>')
+        fixture_row.append('<th>' + home_team + '</th>');
+        fixture_row.append('<th>' + away_team + '</th>');
+        fixture_row.append('<th>' + result.goalsHomeTeam + '</th>');
+        fixture_row.append('<th>' + result.goalsAwayTeam + '</th>');
+        
+
+        if(current_team == home_team && (result.goalsHomeTeam > result.goalsAwayTeam) || 
+           current_team == away_team && (result.goalsAwayTeam > result.goalsHomeTeam)){
+
+          fixture_row.append('<th id="result-col">W</th>');
+
+        }
+        else if(current_team == home_team && (result.goalsHomeTeam < result.goalsAwayTeam) || 
+                current_team == away_team && (result.goalsAwayTeam < result.goalsHomeTeam)){
+
+          fixture_row.append('<th id="result-col">W</th>');
+
+        }
+        else{
+
+          fixture_row.append('<th id="result-col">D</th>');
+        
+        }
+
+        fixture_table.append(fixture_row);
+
+
+      }
+  
+      table_div.append(fixture_table);
+      modal.append(table_div);
+      modal_flag = true;
     });
 }
 
 function displaySquad(){
 
+  var queryURL = $(this).attr('players');
+
+  $.ajax({
+    headers: { 'X-Auth-Token': 'a1343c1c5d024bafb3c6be16564808e2' },
+    url: queryURL,
+    dataType: 'json',
+    type: 'GET',
+  }).done(function(response){
+
+    $('#team-view').empty();
+    modal.show();
+    console.log(response);
+
+    var table_div = $('<div>');
+    table_div.addClass('modal-content');
+    table_div.append('<span class="close">&times;</span>');
+
+    var player_table = $('<table>');
+    player_table.attr('id', 'player-table');
+    player_table.append('<tr><th>Name</th><th>Number</th><th>Position</th><th>Nationality</th></tr>');
+
+    for(var i = 0; i < response.players.length; i++){
+
+      var player_row = $('<tr>');
+      player_row.append('<th>' + response.players[i].name + '</th>');
+      player_row.append('<th>' + response.players[i].jerseyNumber + '</th>');
+      player_row.append('<th>' + response.players[i].position + '</th>');
+      player_row.append('<th>' + response.players[i].nationality + '</th>');
+      player_table.append(player_row);
+    }
+
+    table_div.append(player_table);
+    modal.append(table_div);
+    modal_flag = true;
+
+  });
 }
 
 $(document).on('click', '.competition-btn', displayFixtures);
